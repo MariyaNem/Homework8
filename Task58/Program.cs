52c864963aa1a8b9fe3e43af20671179077c4707
Mariya Nemzorova <nemzorova777@mail.ru>
2023-02-02 23:38:08 +0700

    Fix mistakes

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -35,24 +35,36 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
+int[,] array1 = CreateMatrixRndInt(3, 2, 1, 9);
+PrintMatrix(array1);
+Console.WriteLine();
+int[,] array2 = CreateMatrixRndInt(2, 1, 1, 9);
+PrintMatrix(array2);
+
+if (array1.GetLength(1) != array2.GetLength(0))
+{
+    Console.WriteLine("Данные матрицы нельзя перемножить");
+}
+else
+{
+    Console.WriteLine($"Результирующая матрица:");
+    int[,] twoMatrixProduct = TwoMatrixProduct(array1, array2);
+    PrintMatrix(twoMatrixProduct);
+}
+
 int[,] TwoMatrixProduct(int[,] firstMatrix, int[,] secondMatrix)
 {
-    int[,] productMatrix = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
+    int[,] productMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
     for (int i = 0; i < firstMatrix.GetLength(0); i++)
     {
-        for (int j = 0; j < firstMatrix.GetLength(1); j++)
+        for (int j = 0; j < secondMatrix.GetLength(1); j++)
         {
-           productMatrix[i,j] = firstMatrix[i,j]*secondMatrix[i,j];
+            for (int x = 0; x < secondMatrix.GetLength(0); x++)
+            {
+                productMatrix[i,j] += firstMatrix[i,x]*secondMatrix[x,j] ;
+            }
+           
         }
     }
     return productMatrix;
 }
-
-int[,] array1 = CreateMatrixRndInt(2, 3, 1, 9);
-PrintMatrix(array1);
-Console.WriteLine();
-int[,] array2 = CreateMatrixRndInt(2, 3, 1, 9);
-PrintMatrix(array2);
-Console.WriteLine($"Результирующая матрица:");
-int[,] twoMatrixProduct = TwoMatrixProduct(array1, array2);
-PrintMatrix(twoMatrixProduct);

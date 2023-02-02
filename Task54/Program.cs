ab851c4afda3a8575101feac5da630399c92480b
Mariya Nemzorova <nemzorova777@mail.ru>
2023-02-02 21:23:52 +0700

    Solve task 54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -9,3 +9,59 @@
 // 9 5 3 2
 // 8 4 4 2
 
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5},");
+            else Console.Write($"{matrix[i, j],5}  ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] RowsOrderedArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int x = 0; x < matrix.GetLength(1); x++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) -1; j++)
+            {
+                if (matrix[i, j] > matrix[i, j + 1])
+                {
+                    int temp = matrix[i, j + 1];
+                    matrix[i, j + 1] = matrix[i, j];
+                    matrix[i, j] = temp;
+                }
+
+            }
+        }
+    }
+    return matrix;
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+int[,] orderedArray = RowsOrderedArray(array2D);
+PrintMatrix(orderedArray);
